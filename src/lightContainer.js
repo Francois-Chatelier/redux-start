98b12457487c9b7ecbbb0801320666b1d2df7607
François Chatelier <francois.chatelier@gmail.com>
2021-12-13 11:48:06 +0100

    react redux app

---
+++ src/lightContainer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const mapStateToProps = (state) => ({
+  light: state,
+});
+
+const mapDispatchToProps = (state) => ({
+  onSwitch: () => dispatchEvent({ type: "SWITCH" }),
+});
+
+const LightComponent = ({ light, onSwitch }) => (
+  <div>
+    <p>{light}</p>
+    <button onClick={onSwitch}>Switch</button>
+  </div>
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(LightComponent);
